5f8b94906fe2bb389ad13da73367aceb10799b78
sryang <sarang628@gmail.com>
2025-10-17 12:30:54 +0900

    change logic

--- app/src/main/java/com/example/pinchzoom/MainActivity.kt
+++ app/src/main/java/com/example/pinchzoom/MainActivity.kt
@@ -1,28 +1,27 @@
 package com.example.pinchzoom
 
-//import coil3.compose.AsyncImage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.example.pinchzoom.submodule.pinchzoom.PinchZoomImageLoader
 import com.example.pinchzoom.ui.theme.PinchZoomTheme
-import com.sarang.torang.di.pinchzoom.ImageLoader
-import com.sarang.torang.di.pinchzoom.PinchZoomImageBox
-import com.sarang.torang.di.pinchzoom.PinchZoomableImageData
+import com.sarang.torang.di.pinchzoom.PinchZoomImageBoxSample
+import com.sarang.torang.di.pinchzoom.PinchZoomState
+import com.sarang.torang.di.pinchzoom.pinchZoomAndTransform
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,55 +30,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             PinchZoomTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    PinchZoomExample()
+                        PinchZoomImageBoxSample(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PinchZoomExample() {
-    val imageSize = 200.dp
-    PinchZoomImageBox(imageLoader = coilAsyncImageLoader())
-    { pinchZoomableImage, // 이 이미지 로더를 사용해서 이미지를 로드 해야. 줌 기능과 바깥에 이미지가 확대되는 기능이 적용됨.
-      zoomState ->
-        Box(Modifier.fillMaxSize()) {
-            LazyColumn {
-                items(10) {
-                    Column {
-                        Text(
-                            text = "Pinch Zoom Image Viewer",
-                            fontSize = 30.sp
-                        )
-                        pinchZoomableImage.invoke(
-                            PinchZoomableImageData(
-                                modifier = Modifier
-                                    .height(imageSize),
-                                url = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhkYTY17vrtw3-dlooIu9n7R7mYFgOwyiCwEtJiFJTuxk4sOCKJ-OVaftSPKX7CfONCn2AMMV70TNP9qfo5avZBaMBn4BGS5DW6wPlbRY2ZZRgBXMEI5HbzduVdwj790uDattXfmQtkE8JJ_OptUUDFpCdJZWKVO_mOuL408H4svVQlt58TcjQe8JWfC5g/s1600/app-quality-performance-play-console-insights-meta.png",
-                                contentScale = ContentScale.Crop,
-                                originHeight = imageSize
-                            )
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-/**
- * 핀치줌 라이브라리 제공용 이미지 로더
- */
-fun coilAsyncImageLoader(): ImageLoader {
-    return {
-        AsyncImage(
-            modifier = it.modifier,
-            model = it.url,
-            contentDescription = null,
-            contentScale = it.contentScale
-        )
     }
 }
